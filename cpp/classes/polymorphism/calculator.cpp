34f9afdd073d20cb7ea0835687b9d4d849d001a3
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-03-26 12:15:41 +0300

    we learnt funstion overloading, compiletime polymorhism vs runtime polymorphism

---
+++ cpp/classes/polymorphism/calculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+// compile time polymorhism -> method overloading(same scope )
+// runtime polymorphism -> funstion overiding(parent class and child classes)
+
+//function overloading 
+
+class Calculator{
+    public:
+    int add(int x, int y){
+        return x + y;
+    };
+    float add(float x , float y){
+        return x + y;
+    };
+   int add(int x, int y, int z){
+    return x + y + z;
+   };
+};
+
+int main(){
+    Calculator c1;
+    //static binding and dynamic binding
+    c1.add(3,4);
+    c1.add(3.5,3.8);
+    c1.add(3,4,5);
+    return 0;
+}
