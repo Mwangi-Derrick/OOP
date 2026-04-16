fa06ed50577e28bca5ce55ef85a26b4c1c98c0a5
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-16 19:15:24 +0300

    we will experiment with STLs(standard template libraries)

---
+++ cpp/stl_cpp/stl.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+//stl stands for standard template library
+//Stack and Queue libraries can be prebuilt instead of building themn from scratch
+
+/*?
+Containers , Algorithims and Iterators
+Continers: Stack Qeueu Linked list Maps Sets
+
+Algorithims like getting highest lowest etc
+
+
+
+
+
+*/
+
+
+int main(){
+
+    return 0;
+}
