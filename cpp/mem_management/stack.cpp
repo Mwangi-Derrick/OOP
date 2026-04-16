d9bfb049ab862d969a783a7f37ab77f978a99750
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-16 19:02:12 +0300

    feat: completed the atck implementation

--- cpp/mem_management/stack.cpp
+++ cpp/mem_management/stack.cpp
@@ -3,65 +3,105 @@
 #include <vector>
 using namespace std;
 
-
-class Stack {
+class DynamicStack {
     private:
-    int *arr[5];
-    //track the top most value of stack
-    int top = -1;
-    //as we push we icrement the value top
+        int* arr;        // Dynamic array
+        int capacity;    // Maximum capacity
+        int top;         // Current top index
+    
     public:
-    void push(int *a){
-        top ++;
-        arr[top + 1] = a;
-    };
-
-    int pop(){
-        //if the value is -1 it means the stack is empty
-        //return the top most value
-        if(top == -1){
-          return;
-        }else{
-           //if the stack has values we will remove 
-           delete(arr[0]);
-           top--;
+        // Constructor
+        DynamicStack(int size = 5) {
+            capacity = size;
+            arr = new int[capacity];
+            top = -1;
         }
-    };
-
-    void peep(){
-        //return the top most value in the stack
-        cout << "value" << arr[0];
-    }
-
-    //we can hhave void display
-    void display(){
-        //loop through the elements in the stack and display
-        for (int i= 0; i < 5; i++){
-           cout << arr[i];
-        };
-    };
-
+        
+        // Destructor to free memory
+        ~DynamicStack() {
+            delete[] arr;
+        }
+        
+        // Push operation
+        void push(int value) {
+            if(top >= capacity - 1) {
+                // Resize if needed (optional feature)
+                resize();
+            }
+            top++;
+            arr[top] = value;
+            cout << "Pushed " << value << " to stack" << endl;
+        }
+        
+        // Resize array when full
+        void resize() {
+            capacity *= 2;
+            int* newArr = new int[capacity];
+            for(int i = 0; i <= top; i++) {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+            cout << "Stack resized to capacity: " << capacity << endl;
+        }
+        
+        // Pop operation
+        int pop() {
+            if(top == -1) {
+                cout << "Stack Underflow! Stack is empty" << endl;
+                return -1;
+            }
+            int poppedValue = arr[top];
+            top--;
+            cout << "Popped " << poppedValue << " from stack" << endl;
+            return poppedValue;
+        }
+        
+        // Peek operation
+        int peek() {
+            if(top == -1) {
+                cout << "Stack is empty" << endl;
+                return -1;
+            }
+            return arr[top];
+        }
+        
+        // Display all elements
+        void display() {
+            cout << "Stack (bottom to top): ";
+            if(top == -1) {
+                cout << "Empty";
+            } else {
+                for(int i = 0; i <= top; i++) {
+                    cout << arr[i];
+                    if(i < top) cout << " -> ";
+                }
+            }
+            cout << " | Size: " << (top + 1) << "/" << capacity << endl;
+        }
+        
+        bool isEmpty() { return top == -1; }
+        int size() { return top + 1; }
 };
 
-
-
-int main(){
-    //class array
-    //provide a push and pop method
-    Stack s;
-    int value = 4;
-    s.push(&value);
-
+int main() {
+    DynamicStack s(3);  // Start with small capacity to demonstrate resizing
+    
+    cout << "=== Dynamic Stack Demo ===" << endl;
+    s.push(4);
+    s.push(7);
+    s.push(2);
     s.display();
-
+    
+    cout << "\n--- Pushing beyond initial capacity ---" << endl;
+    s.push(9);  // This will trigger resize
+    s.push(5);
+    s.display();
+    
+    cout << "\n--- Stack operations ---" << endl;
+    cout << "Top value: " << s.peek() << endl;
     s.pop();
-
     s.display();
-
-    s.peep();
-
-    s.display();
-
+    
     return 0;
-
 }
